d648256122b17d5b8afdca28c28c14a5795799ca
Chris Grandin <cgrandin@shaw.ca>
2015-02-17 07:14:43 -0800

    Added appendix for proportion female, based on the 2013 Rock Sole assessment methods.

--- doc/app-equations/buildtex.bat
+++
@@ -1,1 +0,0 @@
-@latex -synctex=1 "%1.tex" && bibtex "%1" && latex "%1.tex" && latex "%1.tex" && dvips "%1.dvi" && ps2pdf "%1.ps"
--- doc/app-equations/cleantex.bat
+++
@@ -1,9 +0,0 @@
-# Clean up the directory after a LaTeX build. Windows version
-del *.aux
-del *.bbl
-del *.blg
-del *.dvi
-del *.log
-del *.pdf
-del *.ps
-del *.toc
