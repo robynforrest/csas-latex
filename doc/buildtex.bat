e68e8e86838decd07df30dfd639eb5b683394b39
Chris Grandin <cgrandin@shaw.ca>
2015-03-25 10:35:39 -0700

    Added figure size to batch script.

--- doc/buildtex.bat
+++ doc/buildtex.bat
@@ -1,2 +1,3 @@
-Rscript -e "library(knitr); opts_chunk$set(dev='cairo_ps', echo=FALSE, results=FALSE, cache=TRUE, fig.path=\"C:/github/csas-latex/doc/figure/\"); knit('./resDoc.Rnw')" 1> knitrOutput.log 2>&1
+Rscript -e "library(knitr);opts_chunk$set(dev='cairo_ps',fig.path=\"C:/github/csas-latex/doc/figure/\", fig.width=8, fig.height=5, echo=FALSE, results=FALSE, message=FALSE, warning=FALSE, results='hide',cache=TRUE);knit('./resDoc.Rnw')" 1> knitrOutput.log 2>&1
+
 @latex -synctex=1 "resDoc.tex" && bibtex "resDoc" && latex "resDoc.tex" && latex "resDoc.tex" && dvips "resDoc.dvi" && ps2pdf "resDoc.ps" 1> latexOutput.log 2>&1
