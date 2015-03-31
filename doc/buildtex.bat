1620612bf7e697dfdc0cde8e15a8a5c2e1f3895b
Chris Grandin <cgrandin@shaw.ca>
2015-03-31 14:26:22 -0700

    Added more figures and changed several sections/subsections. Added more text for the assessment.

--- doc/buildtex.bat
+++ doc/buildtex.bat
@@ -1,4 +1,4 @@
-Rscript -e "library(knitr);opts_chunk$set(dev='cairo_ps',fig.path='C:/github/csas-latex/doc/figure/', fig.dpi=96, fig.width=8, fig.height=5, echo=FALSE, results=FALSE, message=FALSE, warning=FALSE, results='hide', cache=FALSE);knit('./resDoc.Rnw')" 1> knitrOutput.log 2>&1
+Rscript -e "library(knitr);opts_chunk$set(dev='cairo_ps',fig.path='C:/github/csas-latex/doc/figure/', fig.dpi=96, fig.width=7.5, fig.height=4, echo=FALSE, results=FALSE, message=FALSE, warning=FALSE, results='hide', cache=FALSE);knit('./resDoc.Rnw')" 1> knitrOutput.log 2>&1
 REM Next command allows messages, errors, and warnings for debugging purposes.
 REM Rscript -e "library(knitr);opts_chunk$set(dev='cairo_ps',xtable.type='html', fig.path='C:/github/csas-latex/doc/figure/', fig.dpi=96, fig.width=8, fig.height=5, echo=TRUE, results=TRUE, message=TRUE, warning=TRUE, cache=FALSE);knit('./resDoc.Rnw')" 1> knitrOutput.log 2>&1
 
