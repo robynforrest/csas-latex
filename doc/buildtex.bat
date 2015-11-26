0836b270bbde29f408e478c24ae3d32d5041e0bc
robynforrest <robynforrest@gmail.com>
2015-11-26 13:12:46 -0800

    -a

--- doc/buildtex.bat
+++ doc/buildtex.bat
@@ -1,3 +1,3 @@
-Rscript -e "library(knitr);opts_chunk$set(dev='cairo_ps',fig.path='C:/github/csas-latex/doc/figure/', fig.dpi=96, fig.width=7.5, fig.height=4, echo=FALSE, results=FALSE, message=FALSE, warning=FALSE, results='hide', cache=FALSE);knit('./resDoc.Rnw')" 1> knitrOutput.log 2>&1
+Rscript -e "library(knitr);opts_chunk$set(dev='cairo_ps',fig.path='C:/github/csas-latex/doc/figure/', fig.dpi=96, fig.width=7.5, fig.height=4, echo=FALSE, results=FALSE, message=FALSE, warning=FALSE, results='hide', cache=FALSE);knit('./speraTestDoc.Rnw')" 1> knitrOutput.log 2>&1
 
-@latex -synctex=1 "resDoc.tex" && bibtex "resDoc" && latex "resDoc.tex" && latex "resDoc.tex" && dvips "resDoc.dvi" && ps2pdf "resDoc.ps"
+@latex -synctex=1 "speraTestDoc.tex" && bibtex "speraTestDoc" && latex "speraTestDoc.tex" && latex "speraTestDoc.tex" && dvips "speraTestDoc.dvi" && ps2pdf "speraTestDoc.ps"
